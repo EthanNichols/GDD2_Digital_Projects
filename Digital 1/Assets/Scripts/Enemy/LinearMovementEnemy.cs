53fafb80b4321859a36f1b25e2cf0a4c0699b92b
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2020-02-18 18:48:36 -0500

    Starting code for creating enemies

---
+++ Digital 1/Assets/Scripts/Enemy/LinearMovementEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LinearMovementEnemy : EnemyBase
+{
+		public Vector3 movementDirection;
+		public float movementSpeed;
+
+		public LinearMovementEnemy(ColorType colorType) : base(colorType)
+		{
+		}
+
+		private void FixedUpdate()
+		{
+				transform.position += movementDirection * movementSpeed * Time.deltaTime;
+		}
+}
