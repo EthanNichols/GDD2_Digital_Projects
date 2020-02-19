fded251c5fc54b60480431f54ea5c4a08e270c42
LegendRK <rgk8966@g.rit.edu>
2020-02-18 22:53:29 -0500

    Added new BaseObject and PlayerObject

---
+++ Digital 1/Assets/Scripts/BaseObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseObject : MonoBehaviour
+{
+    [SerializeField]
+    protected float xPos = 0.0f, zPos = 0.0f, moveSpeed = 0.0f;
+    protected Collider collision;
+
+    protected virtual void Start()
+    {
+        collision = GetComponent<Collider>();
+        transform.position = new Vector3(xPos, 0, zPos);
+    }
+}
