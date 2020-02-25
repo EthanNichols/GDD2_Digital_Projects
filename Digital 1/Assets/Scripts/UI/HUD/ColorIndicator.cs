da9b131ac6da4b67b7635c8b1d4f0dcce76e652d
cso7654 <55633247+cso7654@users.noreply.github.com>
2020-02-25 15:09:01 -0500

    Add files via upload

---
+++ Digital 1/Assets/Scripts/UI/HUD/ColorIndicator.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class ColorIndicator : MonoBehaviour
+{
+    public Color fillColor;
+    public GameObject colorIndPanel;
+    public GameObject colorIndFill;
+    private UnityEngine.UI.Image fillImage;
+
+    private Color[] testColors = new Color[] {new Color(1, 0, 0, 1), new Color(0, 1, 0, 1), new Color(0, 0, 1, 1)};
+
+    /// <summary>
+    /// Get/Set the color of the color indicator
+    /// </summary>
+    public Color FillColor
+    {
+        get
+        {
+            return fillColor;
+        }
+        set
+        {
+            fillColor = value;
+            UpdateFill();
+        }
+    }
+
+    void Start()
+    {
+        fillImage = colorIndFill.GetComponent<UnityEngine.UI.Image>();
+        UpdateFill();
+    }
+
+    void Update()
+    {
+        //For testing only
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (FillColor.Equals(testColors[0]))
+            {
+                FillColor = testColors[1];
+            }
+            else if (FillColor.Equals(testColors[1]))
+            {
+                FillColor = testColors[2];
+            }
+            else if (FillColor.Equals(testColors[2]))
+            {
+                FillColor = testColors[0];
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (FillColor == testColors[0])
+            {
+                FillColor = testColors[2];
+            }
+            else if (FillColor == testColors[1])
+            {
+                FillColor = testColors[0];
+            }
+            else if (FillColor == testColors[2])
+            {
+                FillColor = testColors[1];
+            }
+        }
+    }
+
+    public void UpdateFill()
+    {
+        fillImage.color = fillColor;
+    }
+}
---
+++ Digital 1/Assets/Scripts/UI/HUD/HealthBar.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject healthBarPanel;
+    public GameObject healthBarFill;
+    private RectTransform healthBarFillTransform;
+
+    public float health;
+    public float maxHealth;
+
+    /// <summary>
+    /// Get/Set the health value of the health bar
+    /// </summary>
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            UpdateFill();
+        }
+    }
+    /// <summary>
+    /// Get/Set the maximum value of the health bar
+    /// </summary>
+    public float MaxHealth
+    {
+        get
+        {
+            return maxHealth;
+        }
+        set
+        {
+            maxHealth = value;
+            UpdateFill();
+        }
+    }
+
+    void Start()
+    {
+        healthBarFillTransform = healthBarFill.GetComponent<RectTransform>();
+        UpdateFill();
+    }
+
+    void Update()
+    {
+        //For testing only
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Health++;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Health--;
+        }
+    }
+
+    /// <summary>
+    /// Set the width of the health bar's colored fill to match the health value
+    /// </summary>
+    void UpdateFill()
+    {
+        float value = health / maxHealth;
+        healthBarFillTransform.transform.localScale = new Vector3(value, 1, 1);
+    }
+}
