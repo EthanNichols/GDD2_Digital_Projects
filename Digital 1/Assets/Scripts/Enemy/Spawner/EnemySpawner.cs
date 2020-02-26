882a5ce53aaafae325efa04ec6569438365f03b4
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2020-02-25 21:59:36 -0500

    Fixed formatting v2

--- Digital 1/Assets/Scripts/Enemy/Spawner/EnemySpawner.cs
+++ Digital 1/Assets/Scripts/Enemy/Spawner/EnemySpawner.cs
@@ -4,62 +4,62 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-		/// <summary>
-		/// Prefab of the enemy that this spawner spawns
-		/// </summary>
-		public Enemy enemyPrefab;
+				/// <summary>
+				/// Prefab of the enemy that this spawner spawns
+				/// </summary>
+				public Enemy enemyPrefab;
 
-		/// <summary>
-		/// The amount of enemies this spawner will spawn before being destroyed
-		/// </summary>
-		public int spawnCount;
+				/// <summary>
+				/// The amount of enemies this spawner will spawn before being destroyed
+				/// </summary>
+				public int spawnCount;
 
-		/// <summary>
-		/// The time between enemy spawns
-		/// </summary>
-		public float timeBetweenSpawns;
-		/// <summary>
-		/// The time since the last enemy spawn
-		/// </summary>
-		private float timeSinceLastSpawn;
+				/// <summary>
+				/// The time between enemy spawns
+				/// </summary>
+				public float timeBetweenSpawns;
+				/// <summary>
+				/// The time since the last enemy spawn
+				/// </summary>
+				private float timeSinceLastSpawn;
 
-		/// <summary>
-		/// The area that this spawner spawns enemies in
-		/// </summary>
-		public Rect spawnRect;
+				/// <summary>
+				/// The area that this spawner spawns enemies in
+				/// </summary>
+				public Rect spawnRect;
 
-		/// <summary>
-		/// Spawn the <see cref="enemyPrefab"/> in the <see cref="spawnRect"/>
-		/// </summary>
-		private void SpawnEnemy()
-		{
-				Enemy newEnemy = Instantiate(enemyPrefab);
-				newEnemy.transform.position = new Vector3(spawnRect.x + Random.Range(-spawnRect.width, spawnRect.width) * 0.5f, 0.0f, spawnRect.y + Random.Range(-spawnRect.height, spawnRect.height) * 0.5f);
-		}
-
-
-		void Update()
-		{
-				timeSinceLastSpawn += Time.deltaTime;
-
-				if (timeSinceLastSpawn >= timeBetweenSpawns)
+				/// <summary>
+				/// Spawn the <see cref="enemyPrefab"/> in the <see cref="spawnRect"/>
+				/// </summary>
+				private void SpawnEnemy()
 				{
-						timeSinceLastSpawn -= timeBetweenSpawns;
-						spawnCount -= 1;
-						SpawnEnemy();
-
-						// Destroy the object if the spawner is done spawning enemies
-						if (spawnCount == 0)
-						{
-								Destroy(gameObject);
-						}
+								Enemy newEnemy = Instantiate(enemyPrefab);
+								newEnemy.transform.position = new Vector3(spawnRect.x + Random.Range(-spawnRect.width, spawnRect.width) * 0.5f, 0.0f, spawnRect.y + Random.Range(-spawnRect.height, spawnRect.height) * 0.5f);
 				}
-		}
 
-		// Draw the rectangle that enemies are spawned in
-		private void OnDrawGizmos()
-		{
-				Gizmos.color = Color.red;
-				Gizmos.DrawWireCube(new Vector3(spawnRect.x, 0.0f, spawnRect.y), new Vector3(spawnRect.width, 0.0f, spawnRect.height));
-		}
+
+				void Update()
+				{
+								timeSinceLastSpawn += Time.deltaTime;
+
+								if (timeSinceLastSpawn >= timeBetweenSpawns)
+								{
+												timeSinceLastSpawn -= timeBetweenSpawns;
+												spawnCount -= 1;
+												SpawnEnemy();
+
+												// Destroy the object if the spawner is done spawning enemies
+												if (spawnCount == 0)
+												{
+																Destroy(gameObject);
+												}
+								}
+				}
+
+				// Draw the rectangle that enemies are spawned in
+				private void OnDrawGizmos()
+				{
+								Gizmos.color = Color.red;
+								Gizmos.DrawWireCube(new Vector3(spawnRect.x, 0.0f, spawnRect.y), new Vector3(spawnRect.width, 0.0f, spawnRect.height));
+				}
 }
--- Digital 1/Assets/Scripts/Player/PlayerShip.cs
+++ Digital 1/Assets/Scripts/Player/PlayerShip.cs
@@ -4,140 +4,140 @@ using UnityEngine;
 
 public class PlayerShip : MonoBehaviour
 {
-		[SerializeField]
-		private float moveSpeed, fireDelay;
+    [SerializeField]
+    private float moveSpeed, fireDelay;
 
-		private Material currentMaterial;
-		private MeshRenderer meshRenderer;
-		private SphereCollider sphereCollider;
+    private Material currentMaterial;
+    private MeshRenderer meshRenderer;
+    private SphereCollider sphereCollider;
 
-		private float fireTimer;
-		private bool canFire;
+    private float fireTimer;
+    private bool canFire;
 
-		/// <summary>
-		/// List of potential color states of the player
-		/// </summary>
-		private enum ColorState
-		{
-				Neutral,
-				Red,
-				Blue,
-				Yellow
-		}
+    /// <summary>
+    /// List of potential color states of the player
+    /// </summary>
+    private enum ColorState
+    {
+        Neutral,
+        Red,
+        Blue,
+        Yellow
+    }
 
-		[SerializeField]
-		private ColorState currentState;
+    [SerializeField]
+    private ColorState currentState;
 
-		/// <summary>
-		/// Contains list of materials for color changing
-		/// </summary>
-		[SerializeField]
-		private Material[] colorRef;
+    /// <summary>
+    /// Contains list of materials for color changing
+    /// </summary>
+    [SerializeField]
+    private Material[] colorRef;
 
 
-		void Start()
-		{
-				currentState = ColorState.Neutral;
-				currentMaterial = GetComponent<Material>();
-				sphereCollider = GetComponent<SphereCollider>();
-				meshRenderer = GetComponent<MeshRenderer>();
-				fireTimer = 0;
-				canFire = true;
-		}
+    void Start()
+    {
+        currentState = ColorState.Neutral;
+        currentMaterial = GetComponent<Material>();
+        sphereCollider = GetComponent<SphereCollider>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        fireTimer = 0;
+        canFire = true;
+    }
 
-		/// <summary>
-		/// Handle/Hold Firing logic
-		/// </summary>
-		void Fire()
-		{
-				if (!canFire || ColorState.Neutral == currentState)
-						return;
+    /// <summary>
+    /// Handle/Hold Firing logic
+    /// </summary>
+    void Fire()
+    {
+        if (!canFire || ColorState.Neutral == currentState)
+            return;
 
-				// fire a bullet
-				Debug.Log("Fire");
+        // fire a bullet
+        Debug.Log("Fire");
 
-				// start the timer based on delay
-				fireTimer = fireDelay;
-		}
+        // start the timer based on delay
+        fireTimer = fireDelay;
+    }
 
-		/// <summary>
-		/// Handle Color State switching, then update Material/Mesh
-		/// </summary>
-		/// <param name="colorIndex"></param>
-		void ColorSwitch(int colorIndex)
-		{
-				switch (colorIndex)
-				{
-						case 1:
-								currentState = ColorState.Red;
-								break;
-						case 2:
-								currentState = ColorState.Blue;
-								break;
-						case 3:
-								currentState = ColorState.Yellow;
-								break;
-						default:
-								currentState = ColorState.Neutral;
-								break;
-				}
+    /// <summary>
+    /// Handle Color State switching, then update Material/Mesh
+    /// </summary>
+    /// <param name="colorIndex"></param>
+    void ColorSwitch(int colorIndex)
+    {
+        switch (colorIndex)
+        {
+            case 1:
+                currentState = ColorState.Red;
+                break;
+            case 2:
+                currentState = ColorState.Blue;
+                break;
+            case 3:
+                currentState = ColorState.Yellow;
+                break;
+            default:
+                currentState = ColorState.Neutral;
+                break;
+        }
 
-				UpdateMaterial();
-		}
+        UpdateMaterial();
+    }
 
-		void UpdateMaterial()
-		{
-				currentMaterial = colorRef[(int)currentState];
-				meshRenderer.material = currentMaterial;
-		}
+    void UpdateMaterial()
+    {
+        currentMaterial = colorRef[(int)currentState];
+        meshRenderer.material = currentMaterial;
+    }
 
-		/// <summary>
-		/// Handle player inputs
-		/// </summary>
-		void HandleInput()
-		{
-				// movement
-				if (Input.GetButton("Horizontal"))
-				{
-						transform.localPosition += Input.GetAxis("Horizontal") * transform.right * Time.deltaTime * moveSpeed;
-				}
+    /// <summary>
+    /// Handle player inputs
+    /// </summary>
+    void HandleInput()
+    {
+        // movement
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.localPosition += Input.GetAxis("Horizontal") * transform.right * Time.deltaTime * moveSpeed;
+        }
 
-				if (Input.GetButton("Vertical"))
-				{
-						transform.localPosition += Input.GetAxis("Vertical") * transform.forward * Time.deltaTime * moveSpeed;
-				}
+        if (Input.GetButton("Vertical"))
+        {
+            transform.localPosition += Input.GetAxis("Vertical") * transform.forward * Time.deltaTime * moveSpeed;
+        }
 
-				// shooting
-				if (Input.GetButton("Jump"))
-				{
-						Fire();
-				}
+        // shooting
+        if (Input.GetButton("Jump"))
+        {
+            Fire();
+        }
 
-				// changing color
-				if (Input.GetKeyDown(KeyCode.L))
-				{
-						ColorSwitch(1);
-				}
-				if (Input.GetKeyDown(KeyCode.Semicolon))
-				{
-						ColorSwitch(2);
-				}
-				if (Input.GetKeyDown(KeyCode.Quote))
-				{
-						ColorSwitch(3);
-				}
-		}
+        // changing color
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ColorSwitch(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Semicolon))
+        {
+            ColorSwitch(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Quote))
+        {
+            ColorSwitch(3);
+        }
+    }
 
-		/// <summary>
-		/// Handle Inputs, check delays between firing
-		/// </summary>
-		void FixedUpdate()
-		{
-				HandleInput();
+    /// <summary>
+    /// Handle Inputs, check delays between firing
+    /// </summary>
+    void FixedUpdate()
+    {
+        HandleInput();
 
-				// Fire Delay Logic
-				if (!canFire)
-						fireTimer -= Time.deltaTime;
-				canFire = fireTimer <= 0;
-		}
+        // Fire Delay Logic
+        if (!canFire)
+            fireTimer -= Time.deltaTime;
+        canFire = fireTimer <= 0;
+    }
 }
