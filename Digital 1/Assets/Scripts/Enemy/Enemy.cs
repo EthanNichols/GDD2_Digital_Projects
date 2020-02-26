59cb9ee5f49bca5b1c036b23f63ce1cac7fcd3db
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2020-02-25 22:06:54 -0500

    Formatting v3

--- Digital 1/Assets/Scripts/Enemy/Enemy.cs
+++ Digital 1/Assets/Scripts/Enemy/Enemy.cs
@@ -4,37 +4,37 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-				public Vector3 movementDirection;
+	public Vector3 movementDirection;
 
-				[Header("Linear Movement")]
-				/// <summary>
-				/// The speed that the enemy moves
-				/// </summary>
-				public float speed;
-				/// <summary>
-				/// The maximum speed this enemy moves at
-				/// </summary>
-				public float maxSpeed;
-				/// <summary>
-				/// Change in <see cref="Speed"/> per tick
-				/// </summary>
-				public float acceleration;
+	[Header("Linear Movement")]
+	/// <summary>
+	/// The speed that the enemy moves
+	/// </summary>
+	public float speed;
+	/// <summary>
+	/// The maximum speed this enemy moves at
+	/// </summary>
+	public float maxSpeed;
+	/// <summary>
+	/// Change in <see cref="Speed"/> per tick
+	/// </summary>
+	public float acceleration;
 
-				//[Header("Rotation Movement")]
-				//public float angularVelocity;
+	//[Header("Rotation Movement")]
+	//public float angularVelocity;
 
-				private void Start()
-				{
-								transform.forward = movementDirection;
-				}
+	private void Start()
+	{
+		transform.forward = movementDirection;
+	}
 
 
-				private void Update()
-				{
-								speed += acceleration * Time.deltaTime;
-								speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
-								//transform.Rotate(Vector3.up, angularVelocity * Time.deltaTime);
+	private void Update()
+	{
+		speed += acceleration * Time.deltaTime;
+		speed = Mathf.Clamp(speed, -maxSpeed, maxSpeed);
+		//transform.Rotate(Vector3.up, angularVelocity * Time.deltaTime);
 
-								transform.position += transform.forward * speed * Time.deltaTime;
-				}
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
 }
