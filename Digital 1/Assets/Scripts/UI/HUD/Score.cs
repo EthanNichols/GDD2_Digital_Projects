891ebce26304b6911485aa0aafb9812a12dd9f0b
Cam Olson <cso7654@ad.rit.edu>
2020-04-09 18:56:23 -0400

    Added Score and Powerup items

---
+++ Digital 1/Assets/Scripts/UI/HUD/Score.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private int points;
+    [SerializeField] private string label = "Score";
+    [SerializeField] private Text text;
+
+    public int Points
+    {
+        get
+        {
+            return points;
+        }
+        set
+        {
+            points = value;
+            UpdateScore();
+        }
+    }
+
+    public string Label
+    {
+        get
+        {
+            return label;
+        }
+        set
+        {
+            label = value;
+            UpdateScore();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void UpdateScore()
+    {
+        text.text = label + ": " + points;
+    }
+}
