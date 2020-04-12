18a70017367c0a9e1bdbb213878355dc40be20bf
Justin Morris (J Mor Personal) <jmortiger@gmail.com>
2020-04-12 15:22:29 -0400

    Follow camera cleanup, inaccurate comment removed

---
+++ Digital 1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour
+{
+	[SerializeField]
+	private new Camera camera;
+
+	[SerializeField]
+	private GameObject playerObj;
+
+	[SerializeField]
+	[Tooltip("This adjusts the camera zoom.")]
+	private float scale = .50f;
+
+	private float initialSize;
+
+	void Start()
+	{
+		camera = gameObject.GetComponent<Camera>();
+		// Ensure Camera is orthographic
+		camera.orthographic = true;
+		initialSize = camera.orthographicSize;
+		camera.orthographicSize = initialSize * scale;
+		if (playerObj == null)
+		{
+			playerObj = GameObject.FindGameObjectWithTag("Player");
+		}
+		Debug.Assert(playerObj != null);
+		transform.position = new Vector3(playerObj.transform.position.x, transform.position.y, playerObj.transform.position.z);
+	}
+
+	void LateUpdate()
+	{
+		// In case scale has changed, update orthographicSize
+		camera.orthographicSize = initialSize * scale;
+		// Update position
+		transform.position = new Vector3(playerObj.transform.position.x, transform.position.y, playerObj.transform.position.z);
+	}
+}
