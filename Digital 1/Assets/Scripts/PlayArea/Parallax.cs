c1fbcc9435b90f96486b4a42f96ba88d6c7a5033
Cam Olson <cso7654@ad.rit.edu>
2020-04-23 01:25:05 -0400

    Added Parallax
    Known bugs: jittery, shield prefab is invisible

---
+++ Digital 1/Assets/Scripts/PlayArea/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private RectTransform background, layer1, layer2;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        background.transform.position = new Vector3(cam.transform.position.x, -1, cam.transform.position.z);
+        layer1.transform.position = new Vector3(cam.transform.position.x / 2, -1, cam.transform.position.z / 2);
+        layer2.transform.position = new Vector3(cam.transform.position.x / 3, -1, cam.transform.position.z / 3);
+    }
+}
