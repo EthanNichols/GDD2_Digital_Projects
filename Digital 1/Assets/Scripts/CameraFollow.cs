45c88e6ceef68ac67be4e5f8c5b649731ed7b2ed
Tori Rossini <tori.rossini42@gmail.com>
2020-04-26 13:02:04 -0400

    Lerp on camera

--- Digital 1/Assets/Scripts/CameraFollow.cs
+++ Digital 1/Assets/Scripts/CameraFollow.cs
@@ -5,38 +5,58 @@ using UnityEngine;
 [RequireComponent(typeof(Camera))]
 public class CameraFollow : MonoBehaviour
 {
-	[SerializeField]
-	private new Camera camera;
+    [SerializeField]
+    private new Camera camera;
 
-	[SerializeField]
-	private GameObject playerObj;
+    [SerializeField]
+    private GameObject playerObj;
 
-	[SerializeField]
-	[Tooltip("This adjusts the camera zoom.")]
-	private float scale = .50f;
+    [SerializeField]
+    [Tooltip("This adjusts the camera zoom.")]
+    private float scale = .50f;
 
-	private float initialSize;
+    private float initialSize;
 
-	void Start()
-	{
-		camera = gameObject.GetComponent<Camera>();
-		// Ensure Camera is orthographic
-		camera.orthographic = true;
-		initialSize = camera.orthographicSize;
-		camera.orthographicSize = initialSize * scale;
-		if (playerObj == null)
-		{
-			playerObj = GameObject.FindGameObjectWithTag("Player");
-		}
-		Debug.Assert(playerObj != null);
-		transform.position = new Vector3(playerObj.transform.position.x, transform.position.y, playerObj.transform.position.z);
-	}
+    [Header("XZ Axis Variables")]
+    [SerializeField]
+    float xzPositionSmoothTime = .03f;
+    float newPosZ, newPosX;
+    Vector3 xzAxisCameraPosition;
 
-	void LateUpdate()
-	{
-		// In case scale has changed, update orthographicSize
-		camera.orthographicSize = initialSize * scale;
-		// Update position
-		transform.position = new Vector3(playerObj.transform.position.x, transform.position.y, playerObj.transform.position.z);
-	}
+    void Start()
+    {
+        camera = gameObject.GetComponent<Camera>();
+        // Ensure Camera is orthographic
+        camera.orthographic = true;
+        initialSize = camera.orthographicSize;
+        camera.orthographicSize = initialSize * scale;
+        if (playerObj == null)
+        {
+            playerObj = GameObject.FindGameObjectWithTag("Player");
+        }
+        Debug.Assert(playerObj != null);
+        transform.position = new Vector3(playerObj.transform.position.x, transform.position.y, playerObj.transform.position.z);
+    }
+
+    void LateUpdate()
+    {
+        if (playerObj != null && camera != null)
+        {
+            // In case scale has changed, update orthographicSize
+            camera.orthographicSize = initialSize * scale;
+
+            CalculateLerpZXPosition();
+            gameObject.transform.position = xzAxisCameraPosition;
+        }
+    }
+
+
+    public void CalculateLerpZXPosition()
+    {
+        newPosZ = Mathf.Lerp(gameObject.transform.position.z, playerObj.transform.position.z, xzPositionSmoothTime);
+        newPosX = Mathf.Lerp(gameObject.transform.position.x, playerObj.transform.position.x, xzPositionSmoothTime);
+
+        xzAxisCameraPosition = new Vector3(newPosX, transform.position.y, newPosZ);
+    }
 }
+
